f1c5cfed10c6896ec53e7f77212330e02fdcd609
poivre <poivre0101@gmail.com>
2022-06-28 18:07:40 +0800

    fix readme
    删除 pyc 文件，避免因编译产生的冗余

--- Cube/cell.py
+++
@@ -1,8 +0,0 @@
-class Cell:
-    def __init__(self, point=None, norm=None, color=None):
-        self.point = point
-        self.norm = norm
-        self.color = color
-
-    def __str__(self):
-        return self.color
